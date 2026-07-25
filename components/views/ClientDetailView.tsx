355fc7221433f9887a6d514ce15d9a98a5de8ffc
Techinfigo <thetechinfigo@gmail.com>
2026-07-25 18:19:56 +0530

    Fix client detail tab content not scrolling when taller than the screen

--- components/views/ClientDetailView.tsx
+++ components/views/ClientDetailView.tsx
@@ -262,7 +262,7 @@ const ClientDetailView: React.FC<ClientDetailViewProps> = ({
           </div>
         </div>
 
-        <div className="flex-1 overflow-y-auto p-4">
+        <div className="flex-1 overflow-y-auto p-4" style={{ maxHeight: 'calc(100vh - 180px)' }}>
           {activeTab === 'Overview' && (
             <div className="space-y-4">
               {(client.agreedServices && client.agreedServices.length > 0) || finance.advancePaid > 0 ? (
